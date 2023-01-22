9316961ccaa45702b34253ffd1bb6c57bd453870
Aliaksandra <a.sinitsyna07@gmail.com>
2023-01-22 20:39:33 +0100

    init: start task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function reverse (n) {
-  
+function reverse (n) {
+  if (n < 0) {
+    let a = n * (-1);
+    return rev(a);
+  } else {
+    return rev(n);
+  }
 }
+
+function rev (a) {
+    let str = a.toString();
+    let arr = str.split("");
+    arr.reverse();
+    let strNew = arr.join("");
+    return Number(strNew);
+}
+
+module.exports = reverse;
+
+console.log(reverse(-123));
